f3f19e4fe295088ab47870a35ee661f18c31fde3
Nicola Paolucci <nick@durdn.com>
2016-06-29 16:15:09 +0200

    Fix GetConfig call
    I got an error following instructions and running:
    
    ```
    caddydev --source $GOPATH/src/github.com/hacdias/caddy-hugo hugo
    ```
    
    The error was:
    
    ```
    caddydev --source $GOPATH/src/github.com/hacdias/caddy-hugo hugo
    Watcher enabled, watching for file changes.
    Building...
    2016/06/29 16:00:32 exit status 2
    ---- COMMAND: go build -o /var/folders/tb/fky_5n917dl9nv_gyrjvs4bh0000gp/T/caddydev139589907
    ---- 
    ---- # github.com/hacdias/caddy-hugo
    ---- /Users/np/go/src/github.com/hacdias/caddy-hugo/setup.go:24: cannot use c.Key (type string) as type *caddy.Controller in argument to httpserver.GetConfig
    ```
    
    I went to check in the source of the caddy project and it seems that passing the whole config instead than the `.Key` fixes the issue.

--- setup.go
+++ setup.go
@@ -1,25 +1,15 @@
 package hugo
 
 import (
-	"io/ioutil"
 	"log"
-	"net/http"
 	"os"
-	"path/filepath"
-	"strings"
 
-	"github.com/hacdias/caddy-filemanager"
-	"github.com/hacdias/caddy-filemanager/config"
-	"github.com/hacdias/caddy-filemanager/directory"
-	"github.com/hacdias/caddy-filemanager/frontmatter"
-	"github.com/hacdias/caddy-hugo/installer"
-	"github.com/hacdias/caddy-hugo/utils/commands"
+	"github.com/hacdias/caddy-hugo/config"
+	"github.com/hacdias/caddy-hugo/tools/commands"
+	"github.com/hacdias/caddy-hugo/tools/hugo"
 	"github.com/mholt/caddy"
 	"github.com/mholt/caddy/caddyhttp/httpserver"
 )
-
-// AssetsURL is the base url for the assets
-const AssetsURL = "/_hugointernal"
 
 func init() {
 	caddy.RegisterPlugin("hugo", caddy.Plugin{
@@ -32,142 +22,38 @@ func init() {
 // middleware thing.
 func setup(c *caddy.Controller) error {
 	cnf := httpserver.GetConfig(c)
-	conf, _ := parse(c, cnf.Root)
-
-	format := "config.toml"
+	conf, _ := config.Parse(c, cnf.Root)
 
 	// Checks if there is an Hugo website in the path that is provided.
 	// If not, a new website will be created.
 	create := true
 
-	if _, err := os.Stat(conf.Root + "config.yaml"); err == nil {
-		format = "yaml"
+	if _, err := os.Stat(conf.Path + "config.yaml"); err == nil {
 		create = false
 	}
 
-	if _, err := os.Stat(conf.Root + "config.json"); err == nil {
-		format = "json"
+	if _, err := os.Stat(conf.Path + "config.json"); err == nil {
 		create = false
 	}
 
-	if _, err := os.Stat(conf.Root + "config.toml"); err == nil {
-		format = "toml"
+	if _, err := os.Stat(conf.Path + "config.toml"); err == nil {
 		create = false
 	}
 
 	if create {
-		err := commands.Run(conf.Hugo, []string{"new", "site", conf.Root, "--force"}, ".")
+		err := commands.Run(conf.Hugo, []string{"new", "site", conf.Path, "--force"}, ".")
 		if err != nil {
 			log.Panic(err)
 		}
 	}
 
-	// Get Default FrontMatter
-	bytes, err := ioutil.ReadFile(filepath.Clean(conf.Root + "/config." + format))
-
-	if err != nil {
-		log.Panic(err)
-	}
-
-	bytes = directory.AppendFrontMatterRune(bytes, format)
-	f, err := frontmatter.Unmarshal(bytes)
-
-	if err != nil {
-		log.Panic(err)
-	}
-
-	if val, ok := f.(map[string]interface{})["metaDataFormat"]; ok {
-		format = val.(string)
-	}
-
 	// Generates the Hugo website for the first time the plugin is activated.
-	go RunHugo(conf, true)
+	go hugo.Run(conf, true)
 
 	mid := func(next httpserver.Handler) httpserver.Handler {
-		return &Hugo{
-			Next:   next,
-			Config: conf,
-			FileManager: &filemanager.FileManager{
-				Next: next,
-				Configs: []config.Config{
-					config.Config{
-						HugoEnabled: true,
-						PathScope:   conf.Root,
-						FrontMatter: format,
-						Root:        http.Dir(conf.Root),
-						BaseURL:     conf.BaseURL,
-						StyleSheet:  conf.Styles,
-					},
-				},
-			},
-		}
+		return &Hugo{Next: next, Config: conf}
 	}
 
 	cnf.AddMiddleware(mid)
 	return nil
-}
-
-// Config is a configuration for managing a particular hugo website.
-type Config struct {
-	Public      string   // Public content path
-	Root        string   // Hugo files path
-	Hugo        string   // Hugo executable location
-	Styles      string   // Admin styles path
-	Args        []string // Hugo arguments
-	BaseURL     string   // BaseURL of admin interface
-	FileManager *filemanager.FileManager
-}
-
-// Parse parses the configuration set by the user so it can be
-// used by the middleware
-func parse(c *caddy.Controller, root string) (*Config, error) {
-	conf := &Config{
-		Public:  strings.Replace(root, "./", "", -1),
-		BaseURL: "/admin",
-		Root:    "./",
-	}
-
-	conf.Hugo = installer.GetPath()
-	for c.Next() {
-		args := c.RemainingArgs()
-
-		switch len(args) {
-		case 1:
-			conf.Root = args[0]
-			conf.Root = strings.TrimSuffix(conf.Root, "/")
-			conf.Root += "/"
-		}
-
-		for c.NextBlock() {
-			switch c.Val() {
-			case "styles":
-				if !c.NextArg() {
-					return conf, c.ArgErr()
-				}
-				tplBytes, err := ioutil.ReadFile(c.Val())
-				if err != nil {
-					return conf, err
-				}
-				conf.Styles = string(tplBytes)
-			case "admin":
-				if !c.NextArg() {
-					return conf, c.ArgErr()
-				}
-				conf.BaseURL = c.Val()
-				conf.BaseURL = strings.TrimPrefix(conf.BaseURL, "/")
-				conf.BaseURL = "/" + conf.BaseURL
-			default:
-				key := "--" + c.Val()
-				value := "true"
-
-				if c.NextArg() {
-					value = c.Val()
-				}
-
-				conf.Args = append(conf.Args, key+"="+value)
-			}
-		}
-	}
-
-	return conf, nil
 }
